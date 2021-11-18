a2b180a71f520e2b14e1869262e1d944e8f926c3
Lucas Yule <lyrma@cin.ufpe.br>
2021-11-17 21:56:00 -0300

    update insercao idioma e generoJogo

---
+++ scripts sql/povoamento/insercao_Idioma.sql
@@ -0,0 +1,59 @@
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Tom Clancy"s Rainbow Six Siege', 'Inglês');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Brawlhalla', 'Inglês');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Assassin"s Creed® Odyssey', 'Inglês');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('PUBG MOBILE', 'Português');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('League Of Legends', 'Português');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Grand Theft Auto V', 'Português');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Red Dead Redemption 2', 'Inglês');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Battlefield 2042', 'Português');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('FIFA 22', 'Inglês');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Dota 2', 'Inglês');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Call of Duty: Black Ops 3', 'Português');
+
+INSERT INTO
+  Idioma (nome_joga, idioma)
+VALUES
+  ('Marvel vs. Capcom Infinite', 'Português');
---
+++ scripts sql/povoamento/insercao_generoJogo.sql
@@ -0,0 +1,59 @@
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Tom Clancy"s Rainbow Six Siege', 'FPS');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Brawlhalla', 'Luta');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Assassin"s Creed® Odyssey', 'Ação');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('PUBG MOBILE', 'FPS');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('League Of Legends', 'MOBA');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Grand Theft Auto V', 'Ação');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Red Dead Redemption 2', 'Aventura');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Battlefield 2042', 'FPS');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('FIFA 22', 'Futebol');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Dota 2', 'MOBA');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Call of Duty: Black Ops 3', 'FPS');
+
+INSERT INTO
+  generoJogo (nome_jogo, genero)
+VALUES
+  ('Marvel vs. Capcom Infinite', 'Luta');
