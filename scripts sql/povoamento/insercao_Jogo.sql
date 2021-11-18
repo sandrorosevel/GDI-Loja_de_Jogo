ded0c76e364f199e0693c56dbaf35bc6aae9dbd4
Lucas Yule <lyrma@cin.ufpe.br>
2021-11-17 21:37:23 -0300

    update insercao jogo

--- scripts sql/povoamento/insercao_Jogo.sql
+++ scripts sql/povoamento/insercao_Jogo.sql
@@ -12,8 +12,29 @@ INSERT INTO
     especificacao_recomendada
   )
 VALUES
-  ('Tom Clancy"s The Division 2', 'RedLynx', 'Ubisoft', '18', '20/01/2020', '', '200', '7.7', '', '',)
-  INSERT INTO
+  (
+    'Tom Clancy"s Rainbow Six Siege',
+    'Ubisoft Montreal',
+    'Ubisoft',
+    '18',
+    '01/12/2015',
+    'https://store.steampowered.com/app/359550/Tom_Clancys_Rainbow_Six_Siege/',
+    '59.90',
+    '7.7',
+    'SO: Windows 7, Windows 8.1, Windows 10 (64bit versions required)
+Processador: Intel Core i3 560 @ 3.3 GHz or AMD Phenom II X4 945 @ 3.0 GHz
+Memória: 6 GB de RAM
+Placa de vídeo: NVIDIA GeForce GTX 460 or AMD Radeon HD 5870 (DirectX-11 compliant with 1GB of VRAM)
+Rede: Conexão de internet banda larga
+Armazenamento: 61 GB de espaço disponível',
+    'SO: Windows 7 SP1, Windows 8.1, Windows 10 (64bit versions required)
+Processador: Intel Core i5-2500K @ 3.3 GHz or better or AMD FX-8120 @ 3.1 Ghz or better
+Memória: 8 GB de RAM
+Placa de vídeo: NVIDIA GeForce GTX 670 (or GTX 760 / GTX 960) or AMD Radeon HD 7970 (or R9 280x [2GB VRAM] / R9 380 / Fury X)
+Rede: Conexão de internet banda larga
+Armazenamento: 61 GB de espaço disponível',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -27,8 +48,22 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Brawlhalla',
+    'Blue Mammoth Games',
+    'Ubisoft',
+    'Livre',
+    '17/10/2017',
+    'https://store.steampowered.com/app/291550/Brawlhalla/',
+    'Gratuito',
+    '8.9',
+    'Memória: 2 GB de RAM
+Armazenamento: 800 MB de espaço disponível',
+    'Memória: 4 GB de RAM
+Rede: Conexão de internet banda larga
+Armazenamento: 2 GB de espaço disponível',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -42,8 +77,28 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Assassin"s Creed® Odyssey',
+    'Ubisoft Quebec',
+    'Ubisoft',
+    '16',
+    '5/10/2018',
+    'https://store.steampowered.com/app/812140/Assassins_Creed_Odyssey/',
+    '179.99',
+    '8.5',
+    'SO: Windows 7 SP1, Windows 8.1, Windows 10 (64bit versions only)
+Processador: AMD FX 6300 @ 3.8 GHz, Ryzen 3 - 1200, Intel Core i5 2400 @ 3.1 GHz (MORE DETAILS HERE)
+Memória: 8 GB de RAM
+Placa de vídeo: AMD Radeon R9 285, NVIDIA GeForce GTX 660 (2GB VRAM with Shader Model 5.0) (MORE DETAILS HERE)
+DirectX: Versão 11
+Armazenamento: 46+ GB de espaço disponível',
+    'SO: Windows 7 SP1, Windows 8.1, Windows 10 (64bit versions only)
+Processador: AMD FX-8350 @ 4.0 GHz, Ryzen 5 - 1400, Intel Core i7-3770 @ 3.5 GHz or better (MORE DETAILS HERE)
+Memória: 8 GB de RAM
+Placa de vídeo: AMD Radeon R9 290, NVIDIA GeForce GTX 970 (4GB VRAM with Shader Model 5.0) or better (MORE DETAILS HERE)
+Armazenamento: 46+ GB de espaço disponível',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -57,8 +112,21 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'PUBG MOBILE',
+    'PUBG Corporation',
+    'Tencent',
+    '14',
+    '20/12/2017',
+    'https://www.pubgmobile.com/',
+    'Gratuito',
+    '7.5',
+    '2 GB
+Sistema Operacional: Android 6.0 ou superior. CPU: 64-bit (ABI arm64 ou superior) RAM: 2 GB ou mais. Open GL 3.1 ou superior / Vulkan 1.1 ou superior.',
+    '2 GB
+Sistema Operacional: Android 6.0 ou superior. CPU: 64-bit (ABI arm64 ou superior) RAM: 4 GB ou mais. Open GL 3.1 ou superior / Vulkan 1.1 ou superior.',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -72,8 +140,33 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'League Of Legends',
+    'Riot Games',
+    'Tencent',
+    'Livre',
+    '27/10/2009',
+    'https://www.leagueoflegends.com/pt-br/',
+    'Gratuito',
+    '9.0',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Requisitos de sistema em breve
+Processador: Intel Core i5-2300, AMD A8-5600k
+Memória: 8 GB de RAM
+Placa de vídeo: Radeon Vega 8 / Intel Iris Plus / Geforce GTX 650 / Radeon HD 7770 (2GB VRAM Required)
+DirectX: Versão 11
+Armazenamento: 15 GB de espaço disponível
+Outras observações: Recommended for 1080p, 30FPS on Medium Settings',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Requisitos de sistema em breve
+Processador: Intel Core i3-6100, AMD FX-8120
+Memória: 8 GB de RAM
+Placa de vídeo: Geforce GTX 660Ti, Radeon HD 7950
+DirectX: Versão 11
+Armazenamento: 15 GB de espaço disponível
+Outras observações: Recommended for 1440p, 60FPS on High Settings',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -87,8 +180,30 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Grand Theft Auto V',
+    'Rockstar North',
+    'Rockstar Games',
+    '16',
+    '14/04/2015',
+    'https://store.steampowered.com/app/271590/Grand_Theft_Auto_V/',
+    '66.66',
+    '10',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1
+Processador: Intel Core 2 Quad CPU Q6600 @ 2.40 GHz (4 CPUs) / AMD Phenom 9850 Quad-Core (4 CPUs) @ 2.5 GHz
+Memória: 4 GB de RAM
+Placa de vídeo: NVIDIA 9800 GT 1 GB / AMD HD 4870 1 GB (DX 10, 10.1, 11)
+Armazenamento: 72 GB de espaço disponível',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1
+Processador: Intel Core i5 3470 @ 3.2 GHZ (4 CPUs) / AMD X8 FX-8350 @ 4 GHZ (8 CPUs)
+Memória: 8 GB de RAM
+Placa de vídeo: NVIDIA GTX 660 2 GB / AMD HD7870 2 GB
+Armazenamento: 72 GB de espaço disponível
+Placa de som: 100% compatível com DirectX 10',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -102,8 +217,31 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Red Dead Redemption 2',
+    'Rockstar Games',
+    'Rockstar Games',
+    '16',
+    '5/12/2019',
+    'https://store.steampowered.com/app/1174180/Red_Dead_Redemption_2/',
+    '180.90',
+    '9.5',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Windows 7 - Service Pack 1 (6.1.7601)
+Processador: Intel® Core™ i5-2500K / AMD FX-6300
+Memória: 8 GB de RAM
+Placa de vídeo: Nvidia GeForce GTX 770 2GB / AMD Radeon R9 280 3GB
+Rede: Conexão de internet banda larga
+Armazenamento: 150 GB de espaço disponível',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Windows 10 - April 2018 Update (v1803)
+Processador: Intel® Core™ i7-4770K / AMD Ryzen 5 1500X
+Memória: 12 GB de RAM
+Placa de vídeo: Nvidia GeForce GTX 1060 6GB / AMD Radeon RX 480 4GB
+Rede: Conexão de internet banda larga
+Armazenamento: 150 GB de espaço disponível',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -117,8 +255,35 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Battlefield 2042',
+    'DICE',
+    'EA Games',
+    '18',
+    '
+19/11/2021',
+    'https://store.steampowered.com/app/1517290/Battlefield_2042/',
+    '242.00',
+    '9.7',
+    'Requer um processador e sistema operacional de 64 bits
+SO: 64-bit Windows 10
+Processador: AMD Ryzen 5 3600, Core i5 6600K
+Memória: 8 GB de RAM
+Placa de vídeo: AMD Radeon RX 560,Nvidia GeForce GTX 1050 Ti
+DirectX: Versão 12
+Rede: Conexão de internet banda larga
+Armazenamento: 100 GB de espaço disponível',
+    'Requer um processador e sistema operacional de 64 bits
+SO: 64-bit Windows 10
+Processador: AMD Ryzen 7 2700X, Intel Core i7 4790
+Memória: 16 GB de RAM
+Placa de vídeo: AMD Radeon RX 6600 XT, Nvidia GeForce RTX 3060
+DirectX: Versão 12
+Rede: Conexão de internet banda larga
+Armazenamento: 100 GB de espaço disponível
+',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -132,8 +297,31 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'FIFA 22',
+    'EA Games Canada',
+    'EA Games',
+    '14',
+    '
+1/10/2021',
+    'https://store.steampowered.com/app/1506830/FIFA_22/',
+    '242.00',
+    '8.7',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Windows 10 - 64-Bit
+Processador: Intel Core i3-6100 @ 3.7GHz or AMD Athlon X4 880K @4GHz
+Memória: 8 GB de RAM
+Placa de vídeo: NVIDIA GTX 660 2GB or AMD Radeon HD 7850 2GB
+Armazenamento: 50 GB de espaço disponível',
+    'Requer um processador e sistema operacional de 64 bits
+SO: Windows 10 - 64-Bit
+Processador: Intel i5-3550 @ 3.40GHz or AMD FX 8150 @ 3.6GHz
+Memória: 8 GB de RAM
+Placa de vídeo: NVIDIA GeForce GTX 670 or AMD Radeon R9 270X
+Rede: Conexão de internet banda larga
+Armazenamento: 50 GB de espaço disponível',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -147,8 +335,33 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Dota 2',
+    'Valve',
+    'Blizzard',
+    'Livre',
+    '9/07/2013',
+    'https://store.steampowered.com/app/570/Dota_2/',
+    'Gratuito',
+    '8.9',
+    'SO: Windows 7 ou posterior
+Processador: Processador com dois núcleos da Intel ou AMD (2,8 GHz)
+Memória: 4 GB de RAM
+Placa de vídeo: NVIDIA GeForce 8600/9600GT, ATI/AMD Radeon HD2600/3600
+DirectX: Versão 9.0c
+Rede: Conexão de internet banda larga
+Armazenamento: 15 GB de espaço disponível
+Placa de som: Compatibilidade com DirectX',
+    'SO: Windows 7 ou posterior
+Processador: Processador com dois núcleos da Intel ou AMD (2,8 GHz)
+Memória: 6 GB de RAM
+Placa de vídeo: NVIDIA GeForce 8600/9600GT, ATI/AMD Radeon HD2600/3600
+DirectX: Versão 9.0c
+Rede: Conexão de internet banda larga
+Armazenamento: 15 GB de espaço disponível
+Placa de som: Compatibilidade com DirectX',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -162,8 +375,27 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
-  INSERT INTO
+  (
+    'Call of Duty: Black Ops 3',
+    'Treyarch',
+    'Activision',
+    '16',
+    '
+6/11/2015',
+    'https://store.steampowered.com/app/311210/Call_of_Duty_Black_Ops_III/',
+    '128.69',
+    '9.4',
+    'SO: Windows 7 64-Bit / Windows 8 64-Bit / Windows 8.1 64-Bit
+Processador: Intel® Core™ i3-530 @ 2.93 GHz / AMD Phenom™ II X4 810 @ 2.60 GHz
+Memória: 6 GB de RAM
+Placa de vídeo: NVIDIA® GeForce® GTX 470 @ 1GB / ATI® Radeon™ HD 6970 @ 1GB
+DirectX: Versão 11
+Rede: Conexão de internet banda larga
+Armazenamento: 100 GB de espaço disponível
+Placa de som: DirectX Compatible',
+    '',
+  )
+INSERT INTO
   Jogo (
     nome,
     desenvolvedora,
@@ -177,4 +409,30 @@ VALUES
     especificacao_recomendada
   )
 VALUES
-  ('', '', '', '', '', '', '', '', '', '',)
+  (
+    'Marvel vs. Capcom Infinite',
+    'Capcom',
+    'Capcom',
+    '12',
+    '
+19/09/2017',
+    'https://store.steampowered.com/app/493840/Marvel_vs_Capcom_Infinite/',
+    '89.00',
+    '7.3',
+    'SO: Windows 7 64-bit
+Processador: Intel Core i3-4160 @ 3.60GHz
+Memória: 6 GB de RAM
+Placa de vídeo: NVIDIA® GeForce® GTX 480, GTX 570, GTX 670, or better
+DirectX: Versão 11
+Rede: Conexão de internet banda larga
+Armazenamento: 59 GB de espaço disponível
+Placa de som: DirectSound Supported (DirectX® 9.0c or more)',
+    'SO: WINDOWS® 7, 8, 8.1, 10 (64-BIT Required)
+Processador: Intel Core i5-4690K @3.50GHz or AMD FX-9370
+Memória: 8 GB de RAM
+Placa de vídeo: NVIDIA® GeForce® GTX 960 or AMD Radeon R7 370
+DirectX: Versão 11
+Rede: Conexão de internet banda larga
+Armazenamento: 59 GB de espaço disponível
+Placa de som: DirectSound Supported (DirectX® 9.0c or more)',
+  )
